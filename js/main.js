476a55e88a86a83162d11c895b48b2bccc330932
Danilo Silva <danilo.silva@msn.com>
2025-05-07 00:16:45 -0300

    refatorar: remover código duplicado e limpar importações no main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 import { criarItemDaLista } from "../scripts/criarItemDaLista.js";
 import { configurarData } from "../utils/configurarData.js";
-<<<<<<< HEAD
 import { verificarListaVazia } from "../utils/verificarListaVazia.js";
-=======
-<<<<<<< HEAD
-import { verificarListaVazia } from "../utils/verificarListaVazia.js";
-=======
->>>>>>> 01cdb36aeed9048860138d7db191b9fe205d0d26
->>>>>>> d005601da85a58af4f184bd778632e0ecd5f2172
 import { adicionarComportamentoDeCompra } from "../scripts/adicionarComportamentoDeCompra.js";
 
 const campoItem = document.querySelector("#campo-item");
@@ -17,15 +10,7 @@ const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
 
 let contador = 0;
 
-<<<<<<< HEAD
 verificarListaVazia(listaCompras, mensagemListaVazia);
-=======
-<<<<<<< HEAD
-verificarListaVazia(listaCompras, mensagemListaVazia);
-=======
-verificarListaVazia();
->>>>>>> 01cdb36aeed9048860138d7db191b9fe205d0d26
->>>>>>> d005601da85a58af4f184bd778632e0ecd5f2172
 
 botaoAdicionar.addEventListener("click", (evento) => {
   evento.preventDefault();
@@ -38,15 +23,7 @@ botaoAdicionar.addEventListener("click", (evento) => {
   const novoItem = criarItemDaLista(campoItem.value, contador++, adicionarComportamentoDeCompra);
   adicionarDataAoItem(novoItem);
   adicionarItemNaLista(novoItem);
-<<<<<<< HEAD
   verificarListaVazia(listaCompras, mensagemListaVazia);
-=======
-<<<<<<< HEAD
-  verificarListaVazia(listaCompras, mensagemListaVazia);
-=======
-  verificarListaVazia();
->>>>>>> 01cdb36aeed9048860138d7db191b9fe205d0d26
->>>>>>> d005601da85a58af4f184bd778632e0ecd5f2172
   limparCampo();
   focarNoCampo();
 });
@@ -65,21 +42,6 @@ function adicionarDataAoItem(item) {
   item.appendChild(dataItem);
 }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-function verificarListaVazia() {
-  const quantidadeItens = listaCompras.querySelectorAll("li").length;
-  if (quantidadeItens === 0) {
-    mensagemListaVazia.classList.remove("invisivel");
-  } else {
-    mensagemListaVazia.classList.add("invisivel");
-  }
-}
-
->>>>>>> 01cdb36aeed9048860138d7db191b9fe205d0d26
->>>>>>> d005601da85a58af4f184bd778632e0ecd5f2172
 function adicionarItemNaLista(item) {
   listaCompras.appendChild(item);
 }
